ab26d23c98645233b4d2449982b84159ca10c202
Tpeytavin <peytavin.thomas@outlook.com>
2020-03-08 19:12:24 +0100

    list fake

--- src/app/tab-list-promos/tab-list-promos.module.ts
+++ src/app/tab-list-promos/tab-list-promos.module.ts
@@ -14,6 +14,9 @@ import { TabListPromosPage } from './tab-list-promos.page';
     FormsModule,
     IonicModule,
     TabListPromosPageRoutingModule
+  ], 
+  providers: [
+    Clipboard
   ],
   declarations: [TabListPromosPage]
 })
